76aba89b3ba1b451aeaa0b71d49aba110e7cc5d6
Rafael Ponte <rafael.ponte@zup.com.br>
2020-09-18 18:49:48 -0300

    adiciona atributo extra na classe Gerente e comentario com dicas;

--- src/main/kotlin/Gerente.kt
+++ src/main/kotlin/Gerente.kt
@@ -2,14 +2,15 @@ class Gerente(
     nome: String,
     cpf: String,
     salario: Double,
-) : Funcionario(nome, cpf, salario) {
+    val senha: String = "123" // tip: atributo adicional
+) : Funcionario(nome, cpf, salario) { // tip: se classe filha tem construtor-primario entao deve chamar construtor-primario da classe pai
 
     override fun bonificacao(): Double { // tip: deve ter "override" para sobrescrever
         return this.salario * 0.3
     }
 
     fun autentica(senha: String): Boolean {
-        return senha == "123";
+        return senha == this.senha;
     }
 
 }
