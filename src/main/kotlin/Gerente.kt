36ca55a893c9c2ede09f4d7e26596813b13f5cd9
Rafael Ponte <rafael.ponte@zup.com.br>
2020-09-24 17:54:10 -0300

    usando interfaces;

--- src/main/kotlin/Gerente.kt
+++ src/main/kotlin/Gerente.kt
@@ -3,13 +3,17 @@ class Gerente(
     cpf: String,
     salario: Double,
     val senha: String = "123" // tip: atributo adicional
-) : Funcionario(nome, cpf, salario) { // tip: se classe filha tem construtor-primario entao deve chamar construtor-primario da classe pai
+) : Funcionario(nome, cpf, salario) // tip: se classe filha tem construtor-primario entao deve chamar construtor-primario da classe pai
+, Autenticavel { // tip: interfaces não possuem construtores (dica pra reconhecer!)
 
     override fun bonificacao(): Double { // tip: deve ter "override" para sobrescrever
         return this.salario * 0.3
     }
 
-    fun autentica(senha: String): Boolean {
+    /**
+     * tip: override explicito mesmo para interfaces
+     */
+    override fun autentica(senha: String): Boolean {
         return senha == this.senha;
     }
 
